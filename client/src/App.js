d1be113bf2514d0df9a282cac3d1f02061f9d21a
Khaled Baghdadi <khaledbagdadi@outook.com>
2020-08-31 11:37:55 +0300

    Dashoard Profile State - Part 2

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,10 @@ import Dashboard from "./components/dashboard/Dashboard";
 import { clearCurrentProfile } from "./actions/profileActions";
 import PrivateRoute from "./components/common/PrivateRoute";
 import CreateProfile from "./components/create-profile/CreateProfile";
+import EditProfile from "./components/edit-profile/EditProfile";
+import AddExperiece from "./components/add-credentials/AddExperience";
 import "./App.css";
+import AddEducation from "./components/add-credentials/AddEducation";
 
 //Check for token
 if (localStorage.jwtToken) {
@@ -55,7 +58,32 @@ function App() {
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
             </Switch>
             <Switch>
-              <PrivateRoute exact path='/create-profile' component={CreateProfile} />
+              <PrivateRoute
+                exact
+                path='/create-profile'
+                component={CreateProfile}
+              />
+            </Switch>
+            <Switch>
+              <PrivateRoute
+                exact
+                path='/edit-profile'
+                component={EditProfile}
+              />
+            </Switch>
+            <Switch>
+              <PrivateRoute
+                exact
+                path='/add-experience'
+                component={AddExperiece}
+              />
+            </Switch>
+            <Switch>
+              <PrivateRoute
+                exact
+                path='/add-education'
+                component={AddEducation}
+              />
             </Switch>
           </div>
           <Footer />
--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import {GET_PROFILE , PROFILE_LOADING, GET_ERRORS, CLEAR_CURRENT_PROFILE} from "./types"
+import {GET_PROFILE , PROFILE_LOADING, GET_ERRORS, CLEAR_CURRENT_PROFILE, SET_CURRENT_USER} from "./types"
 
 export const getCurrentProfile = () => (dispatch) =>{
     dispatch(setProfileLoading());
@@ -33,6 +33,85 @@ export const setProfileLoading=()=>{
         type: PROFILE_LOADING
     }
 }
+// Add experience
+export const addExperience = (expData, history) => dispatch => {
+    axios
+      .post('/api/profile/experience', expData)
+      .then(res => history.push('/dashboard'))
+      .catch(err =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data,
+        })
+      );
+  };
+  // Add education
+export const addEducation = (eduData, history) => dispatch => {
+    axios
+      .post('/api/profile/education', eduData)
+      .then(res => history.push('/dashboard'))
+      .catch(err =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data,
+        })
+      );
+  };
+  // Delete experience
+export const deleteExperience = (id) => dispatch => {
+    if(window.confirm('Are you sure? This can NOT be undone!')) {
+      axios
+        .delete(`/api/profile/experience/${id}`)
+        .then(res => 
+          dispatch({
+            type: GET_PROFILE,
+            payload: res.data,
+          })
+        ).catch(err =>
+          dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data,
+          })
+        );
+    }
+  };
+  // Delete education
+export const deleteEducation = (id) => dispatch => {
+    if(window.confirm('Are you sure? This can NOT be undone!')) {
+      axios
+        .delete(`/api/profile/education/${id}`)
+        .then(res => 
+          dispatch({
+            type: GET_PROFILE,
+            payload: res.data,
+          })
+        ).catch(err =>
+          dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data,
+          })
+        );
+    }
+  };
+
+// Delet account & profile
+export const deleteAccount = () => dispatch => {
+    if(window.confirm('Are you sure? This can NOT be undone!')) {
+      axios
+        .delete('/api/profile')
+        .then(res => 
+          dispatch({
+            type: SET_CURRENT_USER,
+            payload: {},
+          })
+        ).catch(err =>
+          dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data,
+          })
+        );
+    }
+  };
 //Clear profile
 export const clearCurrentProfile=()=>{
     return {
